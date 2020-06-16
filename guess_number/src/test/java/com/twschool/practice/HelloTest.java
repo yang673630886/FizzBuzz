adf54ce95f825f86ac0eaf26a74087f36267320e
yangjingsheng <673630886@qq.com>
2020-06-16 20:50:26 +0800

    add should_return_4A0B_given_1234_when_true1234 test

--- guess_number/src/test/java/com/twschool/practice/HelloTest.java
+++ guess_number/src/test/java/com/twschool/practice/HelloTest.java
@@ -3,10 +3,19 @@ package com.twschool.practice;
 import org.junit.Assert;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class HelloTest {
 
     @Test
     public void should_run_test_pass() {
         Assert.assertTrue(true);
+    }
+
+    @Test
+    public void should_return_4A0B_given_1234_when_true1234(){
+        GuessGame guessGame = new GuessGame();
+        String result = guessGame.answer();
+        assertEquals(result,"4A0B");
     }
 }
