8ed0027ee316e719fd57de291bdef4e2e0b72aa0
yangjingsheng <673630886@qq.com>
2020-06-16 11:16:25 +0800

    should_return_00E_give_00N_R

--- 1592193008185-b08750ec-658f-4b39-be27-d3186331d9da/empty-repo/src/test/java/com/thoughtworks/MarsCarTest.java
+++ 1592193008185-b08750ec-658f-4b39-be27-d3186331d9da/empty-repo/src/test/java/com/thoughtworks/MarsCarTest.java
@@ -6,22 +6,45 @@ import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
 public class MarsCarTest {
+    MarsCar marsCar = new MarsCar(0,0,"N");
     @Test
     public void should_return_01N_give_00N_M(){
-        MarsCar marsCar = new MarsCar(0,0,"N");
+
         MarsCar result = marsCar.move(marsCar,"M");
+
         int x = result.getX_axis();
         int y = result.getY_axis();
         String direction = result.getDirection();
+
         assertEquals(String.valueOf(x),"0");
         assertEquals(String.valueOf(y),"1");
         assertEquals(direction,"N");
     }
+    @Test
     public void should_return_00W_give_00N_L(){
 
-    }
-    public void should_return_00E_give_00N_R(){
+        MarsCar result = marsCar.move(marsCar,"L");
 
+        int x = result.getX_axis();
+        int y = result.getY_axis();
+        String direction = result.getDirection();
+
+        assertEquals(String.valueOf(x),"0");
+        assertEquals(String.valueOf(y),"0");
+        assertEquals(direction,"W");
+
+    }
+    @Test
+    public void should_return_00E_give_00N_R(){
+        MarsCar result = marsCar.move(marsCar,"R");
+
+        int x = result.getX_axis();
+        int y = result.getY_axis();
+        String direction = result.getDirection();
+
+        assertEquals(String.valueOf(x),"0");
+        assertEquals(String.valueOf(y),"0");
+        assertEquals(direction,"E");
     }
 
     public void should_return_0_1S_give_00S_M(){
