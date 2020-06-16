a18c215884d1fc8e94550f91c00403704786ca3b
yangjingsheng <673630886@qq.com>
2020-06-16 21:33:49 +0800

    should_return_randnum_when_randNums

--- guess_number/src/test/java/com/twschool/practice/HelloTest.java
+++ guess_number/src/test/java/com/twschool/practice/HelloTest.java
@@ -3,9 +3,12 @@ package com.twschool.practice;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Arrays;
+
 import static org.junit.Assert.assertEquals;
 
 public class HelloTest {
+    GuessGame guessGame = new GuessGame();
 
     @Test
     public void should_run_test_pass() {
@@ -13,44 +16,44 @@ public class HelloTest {
     }
 
     @Test
-    public void should_return_4A0B_given_1234_when_true1234(){
-        GuessGame guessGame = new GuessGame();
+    public void should_return_4A0B_given_1234_when_true1234() {
         String result = guessGame.answer(new int[]{1, 2, 3, 4}, new int[]{1, 2, 3, 4});
-        assertEquals(result,"4A0B");
+        assertEquals(result, "4A0B");
     }
 
     @Test
-    public void should_return_2A2B_given_1243_when_true1234(){
-        GuessGame guessGame = new GuessGame();
+    public void should_return_2A2B_given_1243_when_true1234() {
         String result = guessGame.answer(new int[]{1, 2, 4, 3}, new int[]{1, 2, 3, 4});
-        assertEquals(result,"2A2B");
+        assertEquals(result, "2A2B");
     }
 
     @Test
-    public void should_return_1A2B_given_1543_when_true1234(){
-        GuessGame guessGame = new GuessGame();
+    public void should_return_1A2B_given_1543_when_true1234() {
         String result = guessGame.answer(new int[]{1, 5, 4, 3}, new int[]{1, 2, 3, 4});
-        assertEquals(result,"1A2B");
+        assertEquals(result, "1A2B");
     }
 
     @Test
-    public void should_return_0A4B_given_4321_when_true1234(){
-        GuessGame guessGame = new GuessGame();
+    public void should_return_0A4B_given_4321_when_true1234() {
         String result = guessGame.answer(new int[]{4, 3, 2, 1}, new int[]{1, 2, 3, 4});
-        assertEquals(result,"0A4B");
+        assertEquals(result, "0A4B");
     }
 
     @Test
-    public void should_return_0A3B_given_5321_when_true1234(){
-        GuessGame guessGame = new GuessGame();
+    public void should_return_0A3B_given_5321_when_true1234() {
         String result = guessGame.answer(new int[]{5, 3, 2, 1}, new int[]{1, 2, 3, 4});
-        assertEquals(result,"0A3B");
+        assertEquals(result, "0A3B");
     }
 
     @Test
-    public void should_return_0A0B_given_5678_when_true1234(){
-        GuessGame guessGame = new GuessGame();
+    public void should_return_0A0B_given_5678_when_true1234() {
         String result = guessGame.answer(new int[]{5, 6, 7, 8}, new int[]{1, 2, 3, 4});
-        assertEquals(result,"0A0B");
+        assertEquals(result, "0A0B");
+    }
+
+    @Test
+    public void should_return_randnum_when_randNums(){
+        int[] result =guessGame.randNums();
+        System.out.println(Arrays.toString(result));
     }
 }
